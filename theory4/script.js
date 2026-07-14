77a936bbc088cbec47ae98e43c72ad064febaa0a
Md. Sazid Uddin <sazid-uddin@aiub.edu>
2026-07-14 08:17:12 +0600

    theory 4

--- theory4/script.js
+++ theory4/script.js
@@ -1,7 +1,28 @@
-alert("Hello from JS");
+// alert("Hello from JS");
 var a = 1.2;
 var b = 5;
 var str = "Hello world";
 var c;
-console.log(typeof(c));
-console.log(a);
+// console.log(typeof(c));
+// console.log(a);
+
+var s;
+// undefined when declared but value not assigned
+console.log(typeof(s));
+console.log(s);
+s = null; // null only when manually assigned
+
+console.log(isNaN(1.5));
+// isNumber(1.5) -> true
+console.log(isNaN("012412441"));
+
+s = "hello world";
+s = 'hello world';
+s = 'My name is "Sazid"';
+s = "My name is 'Sazid'";
+s = "The restaurant's name is \"Pizzaburg\"";
+// escape character
+console.log(s.length);
+console.log(s)
+s = s.replace("Pizzaburg", "Khanas");
+console.log(s);
